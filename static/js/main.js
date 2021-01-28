589d55d709db0de9b1b06b93c655efa17ad0a9c1
Wolfang Torres <wolfang.torres@gmail.com>
2021-01-28 04:21:25 -0400

    rediseño y reorganisacion

---
+++ static/js/main.js
@@ -0,0 +1,54 @@
+// Constants
+const menu = document.querySelector(".menu");
+const close_btn = document.querySelector(".close");
+const navigation = document.querySelector(".navigation");
+
+// Menu Behaviour
+menu.addEventListener("click", () => {
+  navigation.classList.toggle("spread");
+  menu.hidden = true;
+  close_btn.hidden = false;
+});
+
+close_btn.addEventListener("click", () => {
+  navigation.classList.toggle("spread");
+  menu.hidden = false;
+  close_btn.hidden = true;
+});
+
+window.addEventListener("click", (e) => {
+  if (
+    navigation.classList.contains("spread") &&
+    e.target != navigation &&
+    e.target != menu
+  ) {
+    navigation.classList.toggle("spread");
+    menu.hidden = false;
+    close_btn.hidden = true;
+  }
+});
+
+// const gallery = document.querySelectorAll(".gallery");
+// const imagenesLight = document.querySelector(".agregar-imagen");
+// const contenedorLight = document.querySelector(".imagen-light");
+
+// gallery.forEach((imagen) => {
+//   imagen.addEventListener("click", () => {
+//     aparecerImagen(imagen.getAttribute("src"));
+//   });
+// });
+
+// contenedorLight.addEventListener("click", (e) => {
+//   if (e.target !== imagenesLight) {
+//     contenedorLight.classList.toggle("show");
+//     imagenesLight.src.classList.toggle("showImage");
+//     menu.style.opacity = "0";
+//   }
+// });
+
+// const aparecerImagen = (imagen) => {
+//   imagenesLight.src = imagen;
+//   contenedorLight.classList.toggle("show");
+//   imagenesLight.src.classList.toggle("showImage");
+//   menu.style.opacity = "0";
+// };
