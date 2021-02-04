66ee0c8807f203e8c6b9ab00cc09053e646bd10d
Maggnoone <maggnoone@gmail.com>
2021-02-04 10:45:48 -0400

    Se configuro el contador para adaptarse a telf moviles

--- static/js/main.js
+++ static/js/main.js
@@ -62,40 +62,40 @@ const actualizador = setInterval(function(){
   //Definimos el tiempo de inicio de la actividad de la pagina
   
   const inicioClase = new Date('March 1, 2021 10:00:00').getTime();
-  console.log(inicioClase);
+  
   
   //Definimos el tiempo de actual
   
   const tiempoActual = new Date().getTime();
-  console.log(tiempoActual);
+  
   
   //Calculamos la diferencia entre el inicio y el tiempo actual;
   
   let tiempoRestante = inicioClase - tiempoActual;
-  console.log(tiempoRestante);
+  
   //Convertimos los milisegundos en segundos 1s = 1000 ms;
   
   tiempoRestante /=1000;
-  console.log(tiempoRestante);
+  
   //Calculamos los dias restantes
   
   const dias = Math.floor(tiempoRestante/(60*60*24));
-  console.log(dias);
+  
   
   //Calculamos las horas restantes
   
   const horas = Math.floor((tiempoRestante%(60*60*24))/(60*60));
-  console.log(horas);
+  
   
   
   //Calculamos los minutos restantes
   
   const minutos = Math.floor((tiempoRestante%(60*60))/(60));
-  console.log(minutos);
+  
   //Calculamos los segundos restantes
   
   const segundos = Math.floor(tiempoRestante%(60));
-  console.log(segundos);
+  
   
   const contador = document.getElementById('contador');
   
