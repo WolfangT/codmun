a67cecc111de5fde4f894396c70a1acbe537fb53
Maggnoone <maggnoone@gmail.com>
2021-02-06 11:34:45 -0400

    Se agrego galeria de imagen y se modifico el tiempo del contador

--- static/js/main.js
+++ static/js/main.js
@@ -54,70 +54,17 @@ window.addEventListener("click", (e) => {
 // };
 
 
-// CONTADOR
+// Galeria de imagenes
 
+document.querySelectorAll('.contenedor-modal .overlay').forEach((el) => {
+	el.addEventListener('click', function (ev) {
+		ev.stopPropagation();
+		this.parentNode.classList.add('active');
+	});
+});
 
-const actualizador = setInterval(function(){
-
-  //Definimos el tiempo de inicio de la actividad de la pagina
-  
-  const inicioClase = new Date('March 1, 2021 10:00:00').getTime();
-  
-  
-  //Definimos el tiempo de actual
-  
-  const tiempoActual = new Date().getTime();
-  
-  
-  //Calculamos la diferencia entre el inicio y el tiempo actual;
-  
-  let tiempoRestante = inicioClase - tiempoActual;
-  
-  //Convertimos los milisegundos en segundos 1s = 1000 ms;
-  
-  tiempoRestante /=1000;
-  
-  //Calculamos los dias restantes
-  
-  const dias = Math.floor(tiempoRestante/(60*60*24));
-  
-  
-  //Calculamos las horas restantes
-  
-  const horas = Math.floor((tiempoRestante%(60*60*24))/(60*60));
-  
-  
-  
-  //Calculamos los minutos restantes
-  
-  const minutos = Math.floor((tiempoRestante%(60*60))/(60));
-  
-  //Calculamos los segundos restantes
-  
-  const segundos = Math.floor(tiempoRestante%(60));
-  
-  
-  const contador = document.getElementById('contador');
-  
-  
-  //Agregamos los elementos HTML con el valor del tiempo al contenedor con el id contador
-  
-  contador.innerHTML = `
-  <div>
-      <span>${dias}</span>
-      <span class="texto">Dias</span>
-  </div> 
-  <div>
-      <span>${horas}</span>
-      <span class="texto">Horas</span>
-  </div>
-  <div>
-      <span>${minutos}</span>
-      <span class="texto">Minutos</span>
-  </div>
-  <div>
-      <span>${segundos}</span>
-      <span class="texto">Segundos</span>
-  </div>
-  `;
-  }, 1000);
+document.querySelectorAll('.contenedor-modal ').forEach((el) => {
+	el.addEventListener('click', function (ev) {
+		this.classList.remove('active');
+	});
+});
